7d600d147dfbf55c4b0cabc0702e1cd0cfb81dde
Brandon Jared Molina Vazquez <223031@uptapachula.edu.mx>
2024-07-22 10:03:48 -0600

    Changed swagger-ui-express version

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const commerceRouter = require("./proyecto/api/routes/commerceRouter");
 const authRouter = require("./proyecto/api/routes/authRouter");
 const PORT = 5000;
 
-
+app.use(
+    "/api-gambo",
+    swaggerUi.serve,
+    swaggerUi.setup(mainDocs, {
+            customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+            customCss: ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }"
+        }
+    )
+);
 app.use(cors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
@@ -21,14 +29,6 @@ app.get('/', (req, res) => {
     res.status(200).json({message: "Vienvenido a mercachula 🏪"});
 });
 app.use(express.json());
-app.use(
-    "/docs-gambo",
-    swaggerUi.serve,
-    swaggerUi.setup(mainDocs, {
-        customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
-        customCss: ".swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }"
-    })
-);
 app.use('/users', userRouter);
 app.use('/commerce', commerceRouter);
 app.use('/auth', authRouter);
